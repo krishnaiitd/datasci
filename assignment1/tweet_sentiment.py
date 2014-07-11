ca8037fa034b30b1c6cb21ea836cba0e6e69cad0
krishnaiitd <kprasad.iitd@gmail.com>
2014-07-11 09:20:29 +0530

    assignment 1 problem 2 solved

--- assignment1/tweet_sentiment.py
+++ assignment1/tweet_sentiment.py
@@ -1,17 +1,32 @@
 import sys
+import json
 
-def hw():
-    print 'Hello, world!'
-
-def lines(fp):
-    print str(len(fp.readlines()))
-
+def readScores(fb):
+    scores = {};
+    for line in fb:
+        term, score = line.split("\t")
+        scores[term] = int(score)
+    return scores
+def readTweetsText(fb, scores):
+    tweetslist = []
+    for line in fb:
+        sentiment = 0
+        tweet = json.loads(line)
+        if 'text' in tweet.keys():
+            text = tweet['text']
+            wordList = text.encode('utf-8').lower().split()
+            for value in wordList:
+                 if value in scores.keys():
+                     sentiment = sentiment + scores[value]
+        tweetslist.append(sentiment)
+    return tweetslist
 def main():
     sent_file = open(sys.argv[1])
     tweet_file = open(sys.argv[2])
-    hw()
-    lines(sent_file)
-    lines(tweet_file)
+    scores = readScores(sent_file)
+    tweetslist = readTweetsText(tweet_file, scores)
+    for val in tweetslist:
+        print val
 
 if __name__ == '__main__':
     main()
