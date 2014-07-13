d20401cc3ece5f7cf1dc0a23f0492989465803e6
krishnaiitd <kprasad.iitd@gmail.com>
2014-07-13 11:08:31 +0530

    added sample code for assigment 1 problem 4

---
+++ assignment1/happiestState.py
@@ -0,0 +1,10 @@
+import sys
+import json
+
+def main():
+    sent_file = open(sys.argv[1])
+    tweet_file = open(sys.argv[2])
+    print 'Added sample code only, will complete after completation of assignment 2'
+
+if __name__ == '__main__':
+    main()
